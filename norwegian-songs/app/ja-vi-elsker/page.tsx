a773c1edc6ca34a9e59dfc0b36388ef02dfc7599
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2025-05-28 17:37:34 +0200

    great legibility of songs

--- norwegian-songs/app/ja-vi-elsker/page.tsx
+++ norwegian-songs/app/ja-vi-elsker/page.tsx
@@ -15,33 +15,42 @@ export default function JaViElsker() {
 
       <div className="lyrics">
         <p>
+          <span style={{ fontWeight: "bold" }}>1.</span>
+          <br />
           Ja vi elsker dette landet
           <br />
           Som det stiger frem.
           <br />
           Furet værbitt over vannet <br /> Med de tusen hjem <br /> Elsker
-          elsker det og tenker <br /> På vår far og mor <br /> Og den saganatt
-          som senker <br /> Drømmer på vår jord
+          elsker det og tenker <br /> På vår far og mor <br /> /: Og den
+          saganatt som senker <br /> Drømmer på vår jord :/
         </p>
 
         <p>
+          <span style={{ fontWeight: "bold" }}>2.</span>
+          <br />
           Norske mann i hus og hytte <br /> Takk din store gud <br />
           Landet ville han beskytte
           <br /> Skjønt det mørkt så ut
           <br /> Alt hva fedrene har kjempet <br />
           Mødrene har grett
-          <br /> Har den herre stille lempet Så vi vant vår rett
+          <br />
+          /: Har den herre stille lempet <br />
+          Så vi vant vår rett :/
         </p>
 
         <p>
+          <span style={{ fontWeight: "bold" }}>3.</span>
+          <br />
           Ja vi elsker dette landet
           <br /> Som det stiger frem
           <br /> Furet værbitt over vannet
           <br /> Med de tusen hjem
           <br /> Og som fedres kamp har hevet
           <br /> Det av nød til seir
-          <br /> Også vi når det blir krevet
-          <br /> For dets fred slår leir
+          <br />
+          /: Også vi når det blir krevet
+          <br /> For dets fred slår leir :/
         </p>
       </div>
 
--- norwegian-songs/app/norge-i-rodt/page.tsx
+++ norwegian-songs/app/norge-i-rodt/page.tsx
@@ -15,6 +15,8 @@ export default function NorgeIRodt() {
 
       <div className="lyrics">
         <p>
+          <span style={{ fontWeight: "bold" }}>1.</span>
+          <br />
           Hvorhen du går i li og fjell,
           <br />
           en vinterdag, en sommerkveld
@@ -26,9 +28,7 @@ export default function NorgeIRodt() {
           fra havets bryn med fiskevær
           <br />
           og til de hvite skjær,
-        </p>
-
-        <p>
+          <br />
           møter du landet i trefarvet drakt,
           <br />
           svøpt i et gjenskinn av flaggets farveprakt.
@@ -40,6 +40,47 @@ export default function NorgeIRodt() {
           mot den rødmalte stuen ved veien,
           <br />
           det er Norge i rødt, hvitt og blått.
+        </p>
+
+        <p>
+          <span style={{ fontWeight: "bold" }}>2.</span>
+          <br />
+          En vårdag i en solskinnsstund <br />
+          på benken i Studenterlund <br />
+          der sitter han og hun,
+          <br /> to unge, nyutsprungne russ,
+          <br /> to ganske nylig tente bluss,
+          <br /> i 20 grader pluss. <br />
+          Hun er som en gryende forsommerdag <br />
+          som farves av gjenskinnet av det norske flagg. <br />
+          Ja, så hvit som det hvite er kjolen, <br />
+          og så rødt som det rø' hennes kinn, <br />
+          Hennes øyne er blå som fiolen, <br />
+          hun er flagget som vaier i vind. <br />
+          Han har freidig og hvitlugget panne, <br />
+          og en lue i rødt har han fått. <br />
+          Med en lyseblå tiltro til landet står vår ungdom i rødt, hvitt og
+          blått.
+        </p>
+
+        <p>
+          <span style={{ fontWeight: "bold" }}>3.</span>
+          <br />
+          De kjempet både hun og han! <br />
+          Nå lyser seirens baunebrann, <br />
+          utover Norges land. <br />
+          Mot himlen stiger flagg ved flagg som tusen gledesbål i dag, <br />
+          for alle vunne slag. <br />
+          Det knitrer som før over hytte og slott, <br />
+          et flammende merke i rødt og hvitt og blått. <br />
+          Som en regnbuens tegn under skyen, <br />
+          skal det evig i fremtiden stå. <br />
+          Se, det glitrer på ny over byen, <br />
+          i det røde og hvite og blå. <br />
+          La det runge fra gaten og torget, <br />
+          over landet som nordmenn har fått: <br />
+          Du er vårt, du er vårt, gamle Norge! <br />
+          Vi vil kle deg i rødt, hvitt og blått!
         </p>
       </div>
 
